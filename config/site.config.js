52b0bac8affa9fc078c72dbd0268f8ae04f239d1
Y8ii8H <yehaa1024@gmail.com>
2024-09-10 12:56:06 +0900

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -2,12 +2,12 @@
  * This file contains the configuration used for customising the website, such as the folder to share,
  * the title, used Google fonts, site icons, contact info, etc.
  */
-module.exports = {
+module.exports = {    
   // This is what we use to identify who you are when you are initialising the website for the first time.
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'yehaa1024@gmail.com',
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'yeha_1024@outlook.com',
 
 
 
